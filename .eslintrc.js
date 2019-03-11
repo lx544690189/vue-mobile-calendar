60b5741a05899846401338af6f692e58f1494383
lixin <lixin544@163.com>
2019-03-11 23:24:18 +0800

    update 添加规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/require-prop-type-constructor':'off',
   },
   parserOptions: {
     parser: 'babel-eslint'
