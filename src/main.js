9bbcada0d0e9fdead95a1142a9e99583f22d7d00
lixin <lixin544@163.com>
2019-03-17 00:57:20 +0800

    update version

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
+import Calendar from '../lib/install';
 
 Vue.config.productionTip = false;
-
+Vue.use(Calendar);
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
